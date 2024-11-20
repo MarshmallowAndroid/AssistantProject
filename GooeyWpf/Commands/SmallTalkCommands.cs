48044729a941398c27677adab662f2d19c985e74
Jacob Tarun <jacobtarun782@outlook.com>
2024-11-21 00:11:14 +0800

    Fix time of day detection command.

--- GooeyWpf/Commands/SmallTalkCommands.cs
+++ GooeyWpf/Commands/SmallTalkCommands.cs
@@ -52,24 +52,22 @@ namespace GooeyWpf.Commands
 
         public override void Parse(string text)
         {
-            TimeOfDay correctTimeOfDay;
+            TimeOfDay correctTimeOfDay = TimeOfDay.Morning;
             int hour = DateTime.Now.TimeOfDay.Hours;
-            if (hour >= 0 || hour <= 11)
+            if (hour >= 0 && hour <= 11)
                 correctTimeOfDay = TimeOfDay.Morning;
-            else if (hour >= 12 || hour <= 17)
+            if (hour >= 12 && hour <= 17)
                 correctTimeOfDay = TimeOfDay.Afternoon;
-            else if (hour >= 18 || hour <= 23)
+            if (hour >= 18 && hour <= 23)
                 correctTimeOfDay = TimeOfDay.Evening;
-            else
-                correctTimeOfDay = TimeOfDay.Morning;
 
             string mentionedTimeOfDayName = Enum.GetName(typeof(TimeOfDay), mentionedTimeOfDay)?.ToLower() ?? "";
             string correctTimeOfDayName = Enum.GetName(typeof(TimeOfDay), correctTimeOfDay)?.ToLower() ?? "";
             if (mentionedTimeOfDay != correctTimeOfDay)
             {
                 Respond([
-                    $"It's currently {correctTimeOfDayName}, but good {mentionedTimeOfDay} nonetheless.",
-                    $"Thank you, but it's currently {correctTimeOfDayName}. Good {correctTimeOfDayName}"
+                    $"It's currently {correctTimeOfDayName}, but good {mentionedTimeOfDayName} nonetheless.",
+                    $"Thank you, but it's currently {correctTimeOfDayName}. Good {correctTimeOfDayName}."
                     ]);
             }
             else
